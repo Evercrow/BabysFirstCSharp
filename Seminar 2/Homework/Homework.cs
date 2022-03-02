00358e47c1a5cb0b110b9dec62e33ca4793c1d65
Anton Sizykh <toxa-v-s@yandex.ru>
2022-03-02 19:16:41 +0300

    Homework for 2nd seminar

---
+++ Seminar 2/Homework/Homework.cs
@@ -0,0 +1,70 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+/*
+Console.WriteLine("Enter your 3-part number:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+//соскучился по циклу while решил его в интерфейс добавить :)
+while (Math.Abs(num) >= 1000)  //заодно погуглил как взять модуль числа, нашел только через вызов метода Math. Я так понял, у англичан absolute value и переводится как наш "модуль числа"? А Modulus ищет остаток от деления. Жду ваших ответов в оценке домашки, Павел :)
+{
+Console.WriteLine("Entered number is not 3-part, please, try again: ");
+num = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine(ShowSecondPart(num));
+
+int ShowSecondPart(int number)
+{
+   return ((Math.Abs(number%100)/10));
+}
+*/
+
+/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+
+
+/*
+//интересно как эту программу написать, не используя свойства string как массива? Разве что по одной цифре из консоли в числовой массив записывать.
+
+Console.WriteLine("Enter your number:"); 
+string num = Console.ReadLine();        
+//Консоль ругается, что можно получить exception, если в readline подадут null значение. Как это обойти? Есть convert какой-нибудь только для null? 
+
+if (num.Length<3) Console.WriteLine("There is no 3rd part");
+else Console.WriteLine(num[2]);
+*/
+
+
+
+/*
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.WriteLine("Enter your day number:"); 
+int day = Convert.ToInt16(Console.ReadLine());
+
+if ((day < 1) || (day>7))
+   Console.WriteLine("Day with such number does not exist. Switch to gregorian calendar, mate!"); 
+else
+    {
+    if(day > 5)
+    Console.WriteLine("Yes, it is, indeed, weekend.");
+    else
+    Console.WriteLine("No, it's your normal weekday.");
+    }
---
+++ Seminar 2/Program.cs
@@ -0,0 +1,86 @@
+﻿/* int number = new Random().Next(10,99);
+Console.WriteLine("Current number is "+number+" and it's max part is " + ShowMaxNumber(number));
+
+int ShowMaxNumber(int number)
+{
+    int singles = number%10 ;
+    int tens = number/10 ;
+
+    if (tens>singles) 
+    return tens;
+    else 
+    return singles;
+}
+*/
+
+
+//Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.//
+
+/*
+int number = new Random().Next(100,1000);
+Console.WriteLine("Current number is "+number+" and the transformed number is " + RemoveSecondPart(number));
+
+int RemoveSecondPart(int number)
+{
+   int singles = number%10 ;
+   int hundreds = number/100;
+   return (hundreds*10+singles);
+}
+*/
+
+/*Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. 
+Если второе число не кратно числу первому, то программа выводит остаток от деление.
+
+Console.WriteLine("Enter first number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("The remainder of division of two entered numbers equals "+ CheckRemainder(num1,num2));
+*/
+/*
+int CheckRemainder(int num1, int num2)
+{
+    int dividend,divisor,result;
+    if (num1> num2){
+    dividend = num1;
+    divisor = num2;
+    }
+    else {
+    dividend = num2;
+    divisor = num1;
+    }
+
+    result = dividend%divisor;
+
+    if (result == 0)
+    Console.WriteLine(dividend+" is divisible by "+ divisor); 
+    return result;
+}
+*/
+//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно двум введенным пользователем числам.
+Console.WriteLine("Enter number we are checking:");
+int num3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter first divisor number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second divisor number:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+DivisionCheck(num3,num1,num2);
+
+void DivisionCheck(int dividend, int divisor1, int divisor2)
+{
+    if (dividend%divisor1 == 0 && dividend%divisor2 == 0)
+       Console.WriteLine(dividend+" is divisible by both divisor numbers");
+       else 
+        {
+            if  (dividend%divisor1 == 0)
+                Console.WriteLine(dividend+" is divisible by "+divisor1);
+            else
+            {
+                if (dividend%divisor2 == 0)
+                Console.WriteLine(dividend+" is divisible by "+divisor2);
+                else
+                Console.WriteLine(dividend+" is not divisible by "+divisor1+" or "+divisor2);
+            }   
+        }        
+}
