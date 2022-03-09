83d3903eaa0f35e5487d41a6c2560d3fbb0ff43a
Anton Sizykh <toxa-v-s@yandex.ru>
2022-03-09 12:22:15 +0300

    Homework 3 done

--- Seminar 3/Homework/Program.cs
+++ Seminar 3/Homework/Program.cs
@@ -63,11 +63,30 @@ void CheckGenPalindrom(string input)
 A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53
 */
+/*
+double xCoord1,yCoord1,zCoord1,xCoord2,yCoord2,zCoord2 ;
+Console.WriteLine("Enter X coordinate of the first point: ");
+xCoord1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter Y coordinate of the first point: ");
+yCoord1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter Z coordinate of the first point: ");
+zCoord1 = Convert.ToDouble(Console.ReadLine());
 
+Console.WriteLine("Enter X coordinate of the second point: ");
+xCoord2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter Y coordinate of the second point: ");
+yCoord2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter Z coordinate of the second point: ");
+zCoord2 = Convert.ToDouble(Console.ReadLine());
 
+Console.WriteLine("The interval between two points is "+ Find3DInterval(xCoord1,yCoord1,zCoord1,xCoord2,yCoord2,zCoord2));
 
-
-
+double Find3DInterval(double x1,double y1,double z1,double x2,double y2, double z2)
+{   
+ double result = Math.Sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2));
+ return result;
+}
+*/
 
 
 /*
@@ -79,3 +98,21 @@ A (7,-5, 0); B (1,-1,9) -> 11.53
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125
 */
+
+Console.WriteLine("Enter your number");
+int N = Convert.ToInt32(Console.ReadLine());
+PrintCubicTable(N);
+
+void PrintCubicTable(int num)
+{
+    for (int i=1; i<=num ; i++)
+    { 
+      double num_cube = Math.Pow(i,3);  
+      Console.WriteLine("--------+-----------"); 
+      string column1 = "| "+i;
+      column1 = column1.PadRight(8);
+      string column2 = "| "+num_cube;
+      column2 = column2.PadRight(10);
+      Console.WriteLine(column1+column2+" |");  
+    }
+}
