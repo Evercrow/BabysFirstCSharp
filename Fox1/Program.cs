3a23a283d0bc6b60f1074cc1636e1c931661d819
Anton Sizykh <toxa-v-s@yandex.ru>
2022-03-17 23:07:10 +0300

    Polished Fox task1

--- Fox1/Program.cs
+++ Fox1/Program.cs
@@ -6,30 +6,37 @@
   5 2 -3 17 -5 4 0 ->5 и 17 - макс, -5 и -3 - мин.  
  */
 
- int max1=int.MinValue,max2=int.MinValue,min1=int.MaxValue,min2=int.MaxValue; 
- int input,n=0;
- Console.WriteLine("Enter your numbers individually, the program will finish after you enter 0:");
- while (true)
- {
-    input = Convert.ToInt32(Console.ReadLine());   
+int max1 = int.MinValue, max2 = int.MinValue, min1 = int.MaxValue, min2 = int.MaxValue;
+int input, n = 0;
+Console.WriteLine("Enter your numbers individually, the program will finish after you enter 0:");
+while (true)
+{
+    input = Convert.ToInt32(Console.ReadLine());
     if (input == 0) break;
     n++;
-
-    if (max1<=input)
-        {
-            max2 = max1;
-            max1 = input;           
-            
-        }
-
-    if (min2>=input)
+    if (n == 1)
     {
-        min2 = min1;
+        max1 = input;
         min1 = input;
     }
+    else
+    {
+        if (input >= max1)
+        {
+            max2 = max1;
+            max1 = input;
+        }
+        else if (input >= max2) max2 = input;
 
- }
-  
-if(n<3) Console.WriteLine("You entered less than three numbers needed for program to work correctly");
+        if (input <= min1)
+        {
+            min2 = min1;
+            min1 = input;
+        }
+        else if (input <= min2) min2 = input;
+    }
+}
+
+if (n <= 2) Console.WriteLine($"Highest number is {max1} , lowest numbers is {min1}");
 else Console.WriteLine($"Highest numbers are {max1} and {max2}, lowest numbers are {min1} and {min2}");
 // else Console.WriteLine($"Highest numbers are {max1} and {max2}, lowest numbers are {min1} and {min2}. One of the min-max value shown is default,as there is not enough numbers entered for all comparison pairs");
