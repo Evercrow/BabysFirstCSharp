e0482ded3562e706df211eb3fd526ea5e1c0b9b5
Anton Sizykh <toxa-v-s@yandex.ru>
2022-03-23 19:01:00 +0300

    some unsaved changes

--- Seminar 4/Program.cs
+++ Seminar 4/Program.cs
@@ -17,7 +17,7 @@ Console.WriteLine("Введите ваше целое число: ");
 int numA = Convert.ToInt32(Console.ReadLine());
 if (numA < 1) Console.WriteLine("Вы ввели ненатуральное число");
 else Console.WriteLine($"Cумма чисел от 1 до {numA} равна {ArythmSum(numA)}");
-*/
+
 
 // Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
 
@@ -37,5 +37,37 @@ else Console.WriteLine($"Произведение чисел от 1 до {numN} 
 
 
 // Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+void Dlinna(int N)
+{
+    int l = 1;
+    while (Math.Abs(N / 10) > 1)
+    {
+        N = N / 10;
+        l++;
+    }
+    Console.WriteLine("Количество цифр в веденном числе " + l);
+}
 
-// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+Dlinna(number);
+
+
+//Напишите программу, которая выводит массив из 8 элементов, 
+//заполненный нулями и единицами в случайном порядке.
+
+
+void bitArray()
+{
+    int[] a = new int[8];
+
+    for(int i = 0; i < 8; i++)  
+    {
+        a[i] = new Random().Next(0,2);
+        Console.Write(a[i] + " ");
+    }
+}
+
+bitArray();
+
+*/
