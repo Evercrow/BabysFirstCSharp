b80b4c2a9c24b3c8565ebecc2037d9a25b979038
Anton Sizykh <toxa-v-s@yandex.ru>
2022-03-28 20:23:04 +0300

    finished work on homework 6

--- Seminar 6/Homework/Program.cs
+++ Seminar 6/Homework/Program.cs
@@ -46,21 +46,21 @@ int[,] FillBorder(int rows, int columns)
 {
     int[,] border_matrix = new int[rows, columns];
 
-    for (int i = 0; i < columns; i++)
+    for (int i = 0; i < rows; i++)
     {
-        // сразу красим верхнюю и нижнюю границу
-        border_matrix[0, i] = 1;
-        border_matrix[rows - 1, i] = 1;
-        for (int j = 1; j < rows - 1; j++) // исключаем верхнюю и нижнюю границы в условии
+        // сразу красим левую и правую границу
+        border_matrix[i, 0] = 1;
+        border_matrix[i, columns-1] = 1;
+        Console.Write(border_matrix[i,0]+" "); // для вывода нужно не забыть первый и последний столбец. 
+        for (int j = 1; j < columns - 1; j++) // исключаем левую и правую границы в условии цикла
         {
-            border_matrix[j, 0] = 1; // докрасили левую и правую
-            border_matrix[j, columns - 1] = 1;  
+            border_matrix[0, j] = 1; // докрасили верхнюю и нижнюю
+            border_matrix[rows-1, j] = 1;  
             // по умолчанию уже 0 подставлены в пустой integer массив, но попробуем все таки "заполнить" потом, как просят в условии.
-            //border_matrix[j,i] = 0; Здесь снова 0ми станут верхняя и нижняя границы, поэтому без другой записи циклов или if  мне не обойтись.
-            //Также возникли проблемы с печатью, почему-то работает при рядах и столбцах больше 5.
+            //border_matrix[i,j] = 0; Здесь снова 0ми станут левая и правая границы, поэтому без  if  мне не обойтись.
             Console.Write(border_matrix[i, j] + " ");
         }
-        Console.WriteLine(" ");
+        Console.WriteLine(border_matrix[i, columns - 1]+" ");
     }
 
     return border_matrix;
@@ -72,7 +72,7 @@ Console.Write("How many columns you need: ");
 int user_columns = Convert.ToInt16(Console.ReadLine());
 
 int [,] user_matrix = FillBorder(user_rows,user_columns); 
-PrintMatrix(user_matrix);
+// PrintMatrix(user_matrix); -две функции очень пригодились для проверки того, что творится на самом деле с содержимым, когда бился над правильным выводом в консоль.
 
 
 //Теперь попробуем сделать печать и заполнение внутри одной функции, не перекидывая массивы туда-сюда.
