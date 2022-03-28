dbb964f53ad3637772eb5bf8194afd5b87f0c5e4
Anton Sizykh <toxa-v-s@yandex.ru>
2022-03-28 20:06:47 +0300

    cырое ДЗ по 6му уроку

---
+++ Seminar 6/Homework/Program.cs
@@ -0,0 +1,109 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+/*
+int CheckPositive(int end)
+{
+    int positives = 0;
+    for (int i = 0; i < end; i++)
+    {
+        Console.Write($"{i + 1} number: ");
+        int input = Convert.ToInt32(Console.ReadLine());
+        if (input > 0) positives++;
+    }
+    return positives;
+}
+
+Console.WriteLine("How many numbers you intend to check: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("There are " + CheckPositive(M) + " positive numbers.");
+*/
+
+
+
+// Задача 2: Пользователь вводит с клавиатуры кол-во строк и столбцов для массива. Вывести массив в виде матрицы, создав "рамку" из единиц, а внутреннюю зону матрицы заполнить нулями.
+
+// 3, 4 ->
+// 1 1 1 1
+// 1 0 0 1
+// 1 1 1 1
+
+
+
+void PrintMatrix(int[,] printed_matrix)
+{
+    for (int i = 0; i < printed_matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < printed_matrix.GetLength(1); j++) Console.Write(printed_matrix[i, j] + " ");
+        Console.WriteLine(" ");
+    }
+}
+
+
+int[,] FillBorder(int rows, int columns)
+{
+    int[,] border_matrix = new int[rows, columns];
+
+    for (int i = 0; i < columns; i++)
+    {
+        // сразу красим верхнюю и нижнюю границу
+        border_matrix[0, i] = 1;
+        border_matrix[rows - 1, i] = 1;
+        for (int j = 1; j < rows - 1; j++) // исключаем верхнюю и нижнюю границы в условии
+        {
+            border_matrix[j, 0] = 1; // докрасили левую и правую
+            border_matrix[j, columns - 1] = 1;  
+            // по умолчанию уже 0 подставлены в пустой integer массив, но попробуем все таки "заполнить" потом, как просят в условии.
+            //border_matrix[j,i] = 0; Здесь снова 0ми станут верхняя и нижняя границы, поэтому без другой записи циклов или if  мне не обойтись.
+            //Также возникли проблемы с печатью, почему-то работает при рядах и столбцах больше 5.
+            Console.Write(border_matrix[i, j] + " ");
+        }
+        Console.WriteLine(" ");
+    }
+
+    return border_matrix;
+}
+
+Console.Write("How many rows you need: ");
+int user_rows = Convert.ToInt16(Console.ReadLine());
+Console.Write("How many columns you need: ");
+int user_columns = Convert.ToInt16(Console.ReadLine());
+
+int [,] user_matrix = FillBorder(user_rows,user_columns); 
+PrintMatrix(user_matrix);
+
+
+//Теперь попробуем сделать печать и заполнение внутри одной функции, не перекидывая массивы туда-сюда.
+
+
+/*
+void FillBorder(int rows, int columns)
+{
+    int[,] border_matrix = new int[rows, columns];
+
+    for (int i = 0; i < columns; i++)
+    {
+        for (int j = 0; j < rows; j++)
+        {
+            if (j == 0 || j == rows - 1 || i == 0 || i == columns - 1)
+            {
+                border_matrix[j, i] = 1; // красим границы.
+            }
+            else border_matrix[j, i] = 0;
+            Console.Write(border_matrix[j, i] + " ");
+        }
+        Console.WriteLine(" ");
+    }
+
+}
+
+
+Console.Write("How many rows you need: ");
+int user_rows = Convert.ToInt16(Console.ReadLine());
+Console.Write("How many columns you need: ");
+int user_columns = Convert.ToInt16(Console.ReadLine());
+FillBorder(user_rows,user_columns);
+*/
+
