56eebfccb06111d2596e39c0b83244a6e6b5b8b4
Anton Sizykh <toxa-v-s@yandex.ru>
2022-04-14 11:59:20 +0300

    finished with data-info task

--- Seminar 10/Homework/Program.cs
+++ Seminar 10/Homework/Program.cs
@@ -8,7 +8,7 @@
 // Пример:
 
 // входные данные:
-// data = {0, 1, 1, 1, 1, 0, 0, 0, 1 }
+// data = {0, 1, 1, 1, 1, 0, 0, 0, 1  }
 // info = {2, 3, 3, 1 }
 
 // выходные данные:
@@ -16,7 +16,6 @@
 
 
 /*
-
 int[] SplitBinToDec(int[] bin, int[] digitInfo)
 {
     Console.Write("{ ");
@@ -43,6 +42,7 @@ int[] SplitBinToDec(int[] bin, int[] digitInfo)
 // Дорабатываем частные случаи
 
 
+/*
 int[] SplitBinToDec(int[] bin, int[] digitInfo)
 {
     Console.Write("{ ");
@@ -75,14 +75,58 @@ int[] SplitBinToDec(int[] bin, int[] digitInfo)
         Console.Write(number + " ");
         binPosition += digitInfo[pos];
 
-        
+        // Теперь неплохо бы доработать, чтобы в массив записывался  0, если не хватает двоичных цифр.
         result[pos] = number;
     }
     Console.Write("}");
     return result;
 }
+*/
+
+int[] SplitBinToDec(int[] bin, int[] digitInfo)
+{
+    Console.Write("{ ");
+    int[] result = new int[digitInfo.Length];
+    int binPosition = 0, cut = 0;
+    bool isCut = false;
+    for (int pos = 0; pos < digitInfo.Length; pos++)
+    {
+
+        int number = 0;
+        if (binPosition + digitInfo[pos] > bin.Length)
+        {
+            cut = digitInfo[pos] - bin.Length + binPosition;
+            isCut = true;
+            while (binPosition < bin.Length)
+            {
+                number += bin[binPosition] * (int)Math.Pow(2, bin.Length - 1 - binPosition);
+                binPosition++;
+
+            }
+        }
+        else
+        {
+            for (int i = 0; i < digitInfo[pos]; i++)   //количество проходов по циклу равно числу цифр в элементе info
+            {
+                number += bin[binPosition + i] * (int)Math.Pow(2, digitInfo[pos] - 1 - i);
+            }
+        }
+        result[pos] = number;
+        Console.Write(number + " ");
+        binPosition += digitInfo[pos];
+
+    }
+    Console.Write("}");
+    if (isCut)
+    {
+        Console.WriteLine();
+        Console.Write($"Data array was not long enough to source all required binary digits by {cut}");
+    }
+    return result;
+}
 
 
-int[] data = { 0, 1, 1, 1, 1, 0, 0, 0, 1 ,1};
-int[] info = { 2, 3, 4, 2, 2};
+
+int[] data = { 0, 1, 1, 1, 1 , 0, 0, 0, 1 , 1, 1, 0, 1};
+int[] info = { 2, 3, 3, 4 ,2};
 int[] decArray = SplitBinToDec(data, info);
